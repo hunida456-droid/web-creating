3c93d21e9cf07bcb46aedb5e248f5c34da732f27
hunida456 <hunida456@gmail.com>
2026-01-29 06:50:11 +0000

    feat: Expanded dinner options and improved image loading with cache-busting

--- main.js
+++ main.js
@@ -87,7 +87,27 @@ class DinnerRecommender extends HTMLElement {
       { name: "お好み焼き: 関西の魂、キャベツと粉のハーモニー！", imageKeyword: "okonomiyaki" },
       { name: "しゃぶしゃぶ: 湯気の向こうに、至福が待つ！", imageKeyword: "shabu-shabu" },
       { name: "味噌汁: 日本人のDNAに刻まれた、安らぎの味！", imageKeyword: "miso-soup" },
-      { name: "焼き鳥: 串に刺さった小さな幸せ、無限ループの誘惑！", imageKeyword: "yakitori" }
+      { name: "焼き鳥: 串に刺さった小さな幸せ、無限ループの誘惑！", imageKeyword: "yakitori" },
+      { name: "とんかつ: 揚げたてサクサク、ガッツリ元気！", imageKeyword: "tonkatsu" },
+      { name: "そば: 粋な和の香り、ズルズルっと一口！", imageKeyword: "soba" },
+      { name: "おでん: 染み渡る出汁、ホッと一息の温もり！", imageKeyword: "oden" },
+      { name: "餃子: 焼いて良し、茹でて良し、戦いは終わらない！", imageKeyword: "gyoza" },
+      { name: "すき焼き: 豪華絢爛、肉と野菜の夢の共演！", imageKeyword: "sukiyaki" },
+      { name: "刺身: 鮮度命！海の宝石箱や～！", imageKeyword: "sashimi" },
+      { name: "たこ焼き: 外はカリッ、中はトロッ、大阪のソウル！", imageKeyword: "takoyaki" },
+      { name: "もんじゃ焼き: 混ぜて焼いて、未知との遭遇！", imageKeyword: "monjayaki" },
+      { name: "親子丼: 親と子の絆、卵と鶏肉の絶妙ハーモニー！", imageKeyword: "oyakodon" },
+      { name: "カツ丼: 揚げたてカツと出汁の暴力、白米泥棒！", imageKeyword: "katsudon" },
+      { name: "牛丼: 早い、安い、美味い！日本のファストフード王者！", imageKeyword: "gyudon" },
+      { name: "天丼: 揚げたて天ぷら、タレが染みたご飯が最高！", imageKeyword: "tendon" },
+      { name: "エビチリ: プリップリ海老とピリ辛ソース、ご飯が進む！", imageKeyword: "ebichili" },
+      { name: "麻婆豆腐: シビれる辛さ、止まらない旨さ、ご飯が無限！", imageKeyword: "mapo-tofu" },
+      { name: "担々麺: 濃厚スープに痺れる辛さ、中毒性高め！", imageKeyword: "tantanmen" },
+      { name: "ゴーヤチャンプルー: 苦味が旨さに変わる、沖縄の知恵！", imageKeyword: "goya-champuru" },
+      { name: "ちゃんぽん: 魚介の旨味が凝縮、長崎のソウルフード！", imageKeyword: "champon" },
+      { name: "ひつまぶし: 一度で三度美味しい、うなぎの贅沢！", imageKeyword: "hitsumabushi" },
+      { name: "お茶漬け: サラサラっと〆に最高、優しいお味！", imageKeyword: "ochazuke" },
+      { name: "納豆: 粘り強さで健康に！日本人よ、これぞパワーフード！", imageKeyword: "natto" }
     ];
     
     container.innerHTML = '';
@@ -101,7 +121,7 @@ class DinnerRecommender extends HTMLElement {
 
     const recommendationImage = document.createElement('img');
     recommendationImage.setAttribute('class', 'recommendation-image');
-    recommendationImage.src = `https://source.unsplash.com/random/400x300/?${selectedDish.imageKeyword}`;
+    recommendationImage.src = `https://source.unsplash.com/random/400x300/?${selectedDish.imageKeyword}&_=${new Date().getTime()}`;
     recommendationImage.alt = selectedDish.name;
     container.appendChild(recommendationImage);
   }
